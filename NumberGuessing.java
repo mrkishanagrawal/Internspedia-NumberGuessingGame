461ee1283994b593dc8972244dbd9cca484b8242
Kishan Prasad Agrawal <114990579+mrkishanagrawal@users.noreply.github.com>
2022-11-04 15:50:17 +0530

    Update NumberGuessing.java

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,36 +1,52 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class NumberGuessing {
-    public static void
-    guessingNumberGame(){
-        Scanner sc = new Scanner(System.in);
-        int no = 1 + (int)(100 * Math.random());
-        int K = 5;
-        int a, guess;
-        System.out.println("Choose a Number between 1 to 100. Guess the number within 5 trials.");
-        for (a = 0; a < K; a++) {
-            System.out.print("Guess the number :");
-            guess = sc.nextInt();
-            if (no == guess) {
-                System.out.println( "Congratulations! You guessed the number.");
-                break;
-            }
-            else if (no > guess && a != K - 1) {
-                System.out.println("The number is greater than " + guess);
-            }
-            else if (no < guess && a != K - 1) {
-                System.out.println("The number is less than " + guess);
-            }
-        }
-        if (a == K) {
-            System.out.println("You have exhausted K trials.");
-            System.out.println("The number was " + no);
-        }
-        sc.close();
-    }
-    public static void main(String arg[])
-    {
-        guessingNumberGame();
-    }
 
+  public static void main(String[] args) {
+    Random Random_number= new Random();
+    int right_guess=Random_number.nextInt(100);
+    int turns=0;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Guess a number between 1 to 100, You will have 10 turns!" );
+    System.out.println("Best Of Luck!");
+    
+    int guess;
+    int i=0;
+    boolean win=false;
+    while(win==false) {
+    System.out.print("Guess the number :");
+      guess=scan.nextInt();
+      turns++;
+    
+    if(guess==right_guess) {
+      win=true;
+    }
+    else if(i>8){
+      System.out.println("You loose! the right answer was: "+right_guess);
+      return;
+    }
+    else if(guess<right_guess){
+      i++;
+      System.out.println("Your Guess is lower than the right guess! Turns left: "+(10-i));
+      
+      
+      
+    }
+    else if(guess>right_guess) {
+      i++;
+      System.out.println("Your Guess Is Higher Than THe Right Guess! Turns left: "+(10-i));
+      
+    }
+    
+    
+
+  }
+    System.out.println("You win!");
+    System.out.println("Then number was "+right_guess);
+    System.out.println("You used "+turns+" turns to guess the right number");
+    System.out.println("Your score is "+((11-turns)*10)+" out of 100");
+    
+
+}
 }
